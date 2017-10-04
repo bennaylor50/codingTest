982056fc803b1c72b6797c4467ed6f19b73eb456
Ben Naylor <bennaylor@bens-air.home>
2017-10-04 16:06:09 +0100

    news carousel complete

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText : ["<i class='fa fa-arrow-left fa-lg'></i>","<i class='fa fa-arrow-right fa-lg'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+     
+  }
+   
+})
+
