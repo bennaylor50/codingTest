074b01d3d664e203e4be998fc66db6bae498f90d
Ben Naylor <bennaylor@bens-air.home>
2017-10-05 12:39:42 +0100

    calender added

--- js/carousel.js
+++ js/carousel.js
@@ -1,20 +1,43 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    navText : ["<i class='fa fa-arrow-left fa-lg'></i>","<i class='fa fa-arrow-right fa-lg'></i>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:3
-        }
-     
-  }
+$(document).ready(function(){
+
+    $('.owl-news').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-arrow-left fa-lg'></i>","<i class='fa fa-arrow-right fa-lg'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+         
+      }
    
-})
+    })
+
+    $('.owl-calender').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-arrow-left fa-lg'></i>","<i class='fa fa-arrow-right fa-lg'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+         
+      }
+   
+    })
+});
 
