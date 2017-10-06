bb2b447973450f8cba49f5c737be2924cac33062
Ben Naylor <bennaylor@Bens-MacBook-Air.local>
2017-10-06 11:14:27 +0100

    change to welcome

--- js/carousel.js
+++ js/carousel.js
@@ -37,6 +37,25 @@ $(document).ready(function(){
             }
          
       }
+    })
+    
+      $('.owl-follow').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-arrow-left fa-lg'></i>","<i class='fa fa-arrow-right fa-lg'></i>"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+         
+      }
    
     })
 });
